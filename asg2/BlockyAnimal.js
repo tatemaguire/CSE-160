f9460f7627f5ab4663013779f60e729b4c81a80d
tatemaguire <tatemaguir@gmail.com>
2026-04-22 21:26:24 -0700

    drawing a cube using buffers and drawElements! yippee!

--- asg2/BlockyAnimal.js
+++ asg2/BlockyAnimal.js
@@ -17,6 +17,34 @@ void main()
 }
 `;
 
+// Cube Vertices
+const cubeVertices = new Float32Array([
+    -0.5, -0.5, -0.5,
+    0.5, -0.5, -0.5,
+    0.5, -0.5, 0.5,
+    -0.5, -0.5, 0.5,
+    -0.5, 0.5, -0.5,
+    0.5, 0.5, -0.5,
+    0.5, 0.5, 0.5,
+    -0.5, 0.5, 0.5,
+]);
+
+// Cube Face Indices
+const cubeFaces = new Uint8Array([
+    0, 1, 2,
+    0, 2, 3,
+    0, 5, 1,
+    0, 4, 5,
+    0, 3, 7,
+    0, 7, 4,
+    1, 6, 2,
+    1, 5, 6,
+    2, 7, 3,
+    2, 6, 7,
+    4, 6, 5,
+    4, 7, 6
+]);
+
 // Canvas/Context References
 let canvas;
 let gl;
@@ -25,9 +53,17 @@ let gl;
 let a_Position;
 let u_FragColor;
 
+// Buffer Locations
+let cube_array_buffer;
+let cube_element_buffer;
+
+
 function main() {
     initProgram();
+    initBuffers();
+    drawCube();
 }
+
 
 // Sets up canvas, gl, and gets shader variable locations
 function initProgram() {
@@ -80,4 +116,52 @@ function initProgram() {
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+}
+
+
+// Creates, Binds, and Writes to array and element buffers
+function initBuffers() {
+    // -------------------------
+    // Init Array Buffer
+    // -------------------------
+
+    cube_array_buffer = gl.createBuffer();
+    if (!cube_array_buffer) {
+        console.error('Cube Vertex Buffer could not be created');
+        return;
+    }
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, cube_array_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, cubeVertices, gl.STATIC_DRAW);
+
+    // -------------------------
+    // Init Element Buffer
+    // -------------------------
+
+    cube_element_buffer = gl.createBuffer();
+    if (!cube_element_buffer) {
+        console.error('Cube Element Buffer could not be created');
+        return;
+    }
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cube_element_buffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, cubeFaces, gl.STATIC_DRAW);
+}
+
+
+// Draw a Cube
+function drawCube() {
+    // Only necessary if cube wasn't the last thing drawn
+    gl.bindBuffer(gl.ARRAY_BUFFER, cube_array_buffer);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cube_element_buffer);
+
+    // Connect attributes to buffer
+    gl.enableVertexAttribArray(a_Position);
+    gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
+
+    // Set uniform variables
+    gl.uniform4f(u_FragColor, 1.0, 0.0, 0.0, 1.0);
+
+    // Draw cube according to position indices in cube_element_buffer
+    gl.drawElements(gl.TRIANGLES, cubeFaces.length, gl.UNSIGNED_BYTE, 0);
 }
