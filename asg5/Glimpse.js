b4e1a47c36f3e0e26af34a27b8351cd4bcae0267
tatemaguire <tatemaguir@gmail.com>
2026-06-03 15:46:46 -0700

    reorganized initialization

--- asg5/Glimpse.js
+++ asg5/Glimpse.js
@@ -5,36 +5,42 @@ import { Mountain } from "./Mountain.js";
 const canvas = document.querySelector('#c');
 const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
 
-const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 50);
-camera.position.z = 4;
+let camera;
+let controls;
+let scene;
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enablePan = false;
-controls.minDistance = 1;
-controls.maxDistance = 30;
-controls.enableDamping = true;
-controls.dampingFactor = 0.15;
-controls.update();
-
-const scene = new THREE.Scene();
-
-const dirLight = new THREE.DirectionalLight(0xFFFFFF, 3);
-dirLight.position.set(-1, 2, 4);
-scene.add(dirLight);
-
-const ambLight = new THREE.AmbientLight(0xFFFFFF, 0.1);
-scene.add(ambLight);
-
-const mtn = new Mountain();
-scene.add(mtn);
-
+main();
 
 function main() {
+    camera = new THREE.PerspectiveCamera(75, 2, 0.1, 50);
+    camera.position.z = 4;
 
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.enablePan = false;
+    controls.minDistance = 1;
+    controls.maxDistance = 30;
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.15;
+    controls.update();
+
+    scene = new THREE.Scene();
+    buildScene();
 
     requestAnimationFrame(tick)
 }
-main();
+
+
+function buildScene() {
+    const dirLight = new THREE.DirectionalLight(0xFFFFFF, 3);
+    dirLight.position.set(-1, 2, 4);
+    scene.add(dirLight);
+
+    const ambLight = new THREE.AmbientLight(0xFFFFFF, 0.1);
+    scene.add(ambLight);
+
+    const mtn = new Mountain();
+    scene.add(mtn);
+}
 
 
 function tick(time) {
