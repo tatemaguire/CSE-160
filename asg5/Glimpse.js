6bf6b41b3691dcde3a8903f192ce38893f80ab12
tatemaguire <tatemaguir@gmail.com>
2026-06-03 01:36:26 -0700

    animating cube example

--- asg5/Glimpse.js
+++ asg5/Glimpse.js
@@ -1,21 +1,37 @@
 import * as THREE from 'three';
 
+
+const canvas = document.querySelector('#c');
+const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
+
+const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 5);
+camera.position.z = 2;
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry();
+const blueMaterial = new THREE.MeshPhongMaterial({color: 0x2211aa});
+const cube = new THREE.Mesh(geometry, blueMaterial);
+scene.add(cube);
+
+const light = new THREE.DirectionalLight(0xFFFFFF, 3);
+light.position.set(-1, 2, 4);
+scene.add(light);
+
+
 function main() {
-    const canvas = document.querySelector('#c');
-    const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
+    requestAnimationFrame(tick)
+}
+main();
 
-    const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 5);
-    camera.position.z = 2;
 
-    const scene = new THREE.Scene();
+function tick(time) {
+    time *= 0.001;
 
-    const geometry = new THREE.BoxGeometry();
-    const blueMaterial = new THREE.MeshBasicMaterial({color: 0x3310AA})
-    const cube = new THREE.Mesh(geometry, blueMaterial);
-
-    scene.add(cube);
+    cube.rotation.y = time;
+    cube.rotation.x = time;
 
     renderer.render(scene, camera);
-}
 
-main();
+    requestAnimationFrame(tick);
+}
