a193d0ed909dcccb308169bc7ff71e34d96d1150
tatemaguire <tatemaguir@gmail.com>
2026-06-03 01:22:05 -0700

    eslint config and basic scene

--- asg5/Glimpse.js
+++ asg5/Glimpse.js
@@ -1,3 +1,21 @@
 import * as THREE from 'three';
 
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+function main() {
+    const canvas = document.querySelector('#c');
+    const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
+
+    const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 5);
+    camera.position.z = 2;
+
+    const scene = new THREE.Scene();
+
+    const geometry = new THREE.BoxGeometry();
+    const blueMaterial = new THREE.MeshBasicMaterial({color: 0x3310AA})
+    const cube = new THREE.Mesh(geometry, blueMaterial);
+
+    scene.add(cube);
+
+    renderer.render(scene, camera);
+}
+
+main();
--- asg5/eslint.config.js
+++ asg5/eslint.config.js
@@ -1,9 +1,18 @@
 import js from "@eslint/js";
+import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig({
+    languageOptions: {
+        globals: {
+            ...globals.browser
+        }
+    },
     plugins: {
         js
     },
-    extends: ["js/recommended"]
+    extends: ["js/recommended"],
+    rules: {
+        "no-unused-vars": "warn"
+    }
 })
